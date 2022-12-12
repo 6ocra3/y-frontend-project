306c8b31bf39ff24cd80f4ac0677d347b4ef5c2d
JeeEssEm <greendiesel228@gmail.com>
2022-12-13 01:55:43 +0300

    создал header и footer. Добавил react-router-dom в app.jsx и одну тестовую страницу.

---
+++ src/Elements/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import styles from './styles.module.css';
+
+
+export const Footer = () => {
+    return <div className={styles.root}>
+        <p><span className={styles.muted}>Время создания проекта</span> 01:48 13.12.2022</p>
+    </div>
+}
---
+++ src/Elements/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import {Header} from "../Header/Header";
+import {Footer} from "../Footer/Footer";
+
+export const Layout = (props) => {
+
+    return (
+        <div>
+            <Header/>
+            <main>{props.children}</main>
+            <Footer/>
+        </div>
+    )
+}
