60309fdb67466fd0fea2befa4f579314f09d6ba1
JeeEssEm <greendiesel228@gmail.com>
2022-12-13 15:31:47 +0300

    исправил header и footer

--- src/Elements/Footer/Footer.jsx
+++ src/Elements/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css';
 
 
 export const Footer = () => {
-    return <div className={styles.root}>
-        <p><span className={styles.muted}>Время создания проекта</span> 01:48 13.12.2022</p>
-    </div>
+    return <footer className={styles.footer}>
+        <p><span className={styles.footer__muted}>Время создания проекта</span> 01:48 13.12.2022</p>
+    </footer>
 }
--- src/Elements/Header/Header.jsx
+++ src/Elements/Header/Header.jsx
@@ -4,17 +4,19 @@ import { NavLink } from "react-router-dom";
 
 export const Header = () => {
     return (
-        <div className={styles.root}>
+        <header className={styles.header}>
             <NavLink to="/" className={
-                ({isActive}) => isActive ? styles.active : styles.btn
+                ({isActive}) => isActive ? styles.header__button + " " + styles.header__button_active :
+                    styles.header__button
             }
             >Первая страница</NavLink>
 
             <NavLink to="/2" className={
-                ({isActive}) => isActive ? styles.active : styles.btn
+                ({isActive}) => isActive ? styles.header__button + " " + styles.header__button_active :
+                    styles.header__button
             }
             >Вторая страница</NavLink>
-        </div>
+        </header>
     )
 }
 
