0547d6c522e2002b0d73a6312e0e7e37c491bab5
sherstnew <sherstnev.denis.v@gmail.com>
2022-12-14 07:52:05 +0300

    fixes

--- src/Elements/DeveloperCard/DeveloperCard.jsx
+++ src/Elements/DeveloperCard/DeveloperCard.jsx
@@ -1,19 +1,20 @@
 import s from './DeveloperCard.module.scss';
-import github from './icons/github.svg'
-import telegram from './icons/telegram.svg'
+import github_icon from '../../static/icons/github.svg'
+import telegram_icon from '../../static/icons/telegram.svg'
 
-export function DeveloperCard(props) {
+export function DeveloperCard({info}) {
+  const { image, name, github, telegram, bio } = info
   return (
     <div className={s.developer}>
-      <img className={s.developer__img} src={props.bio.image} alt="Фото разработчика" />
+      <img className={s.developer__img} src={image} alt="Фото разработчика" />
       <div className={s.developer__contacts}>
-        <div className="contacts__name">{props.bio.name}</div>
+        <div className="contacts__name">{name}</div>
         <div className="contacts__social">
-          <a href={props.bio.github}><img className={s.icon} src={github} alt="Гитхаб" /></a>
-          <a href={props.bio.telegram}><img className={s.icon} src={telegram} alt="Телеграм" /></a>
+          <a href={github}><img className={s.icon} src={github_icon} alt="Гитхаб" /></a>
+          <a href={telegram}><img className={s.icon} src={telegram_icon} alt="Телеграм" /></a>
         </div>
       </div>
-      <div className={s.developer__bio}>{props.bio.bio}</div>
+      <div className={s.developer__bio}>{bio}</div>
     </div>
   )
 }
