aaaf8f12dbe041cb0d74a2c35485b576723bb887
6ocra3 <6o3kmm@mail.ru>
2022-12-15 17:37:16 +0300

    some fixes

--- src/Elements/YandexApi/YandexMap.jsx
+++ src/Elements/YandexApi/YandexMap.jsx
@@ -9,7 +9,7 @@ export function YandexMap() {
     };
     return (
         <YMaps className={s.blackWhite}>
-            <Map options={{ copyrightLogoVisible: false, copyrightProvidersVisible: false, yandexMapAutoSwitch: false, copyrightUaVisible: false }} defaultState={defaultState} style={{ filter: "grayscale(1)", width: "800px", height: "600px" }} modules={["control.ZoomControl", "control.FullscreenControl"]}>
+            <Map setCenter={() => { console.log(1) }} options={{ copyrightLogoVisible: false, copyrightProvidersVisible: false, yandexMapAutoSwitch: false, copyrightUaVisible: false }} defaultState={defaultState} style={{ filter: "grayscale(1)", width: "800px", height: "600px" }} modules={["control.ZoomControl", "control.FullscreenControl"]}>
                 <Placemark modules={["geoObject.addon.balloon"]} geometry={[55.684758, 37.738521]} />
             </Map>
         </YMaps>
