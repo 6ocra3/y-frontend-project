668d90bc2a9ceffafba595e69a39c5468f88d1ed
sherstnew <sherstnev.denis.v@gmail.com>
2022-12-17 22:31:27 +0300

    new design

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,13 @@
+import { Button } from '../../Elements/Button';
+import FeatherIcon from 'feather-icons-react';
+import styles from './Feedback.module.scss';
+
+export const Feedback = ({setModalActive}) => {
+  return (
+    <section className={styles.feedback}>
+      <h2 className={styles.feedback__header}>Нужен сайт? Интернет-магазин? Другое?</h2>
+      <h3 className={styles.feedback__subheader}>Мы с радостью готовы выполнить ваш заказ! <br /> Просто нажмите на кнопку ниже <br /> <FeatherIcon icon="arrow-down" className={styles.arrowDownIcon}/> </h3>
+      <Button type="dark" onClick={() => setModalActive(true)}>Форма обратной связи</Button>
+    </section>
+  )
+}
