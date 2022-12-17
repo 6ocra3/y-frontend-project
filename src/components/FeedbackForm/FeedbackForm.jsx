e04dcb0e0b848460348600ca271bde652ee533e0
6ocra3 <6o3kmm@mail.ru>
2022-12-17 21:24:05 +0300

    add checkbox rule

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,18 +1,19 @@
 import styles from "./styles.module.css";
 import FeatherIcon from 'feather-icons-react';
-import {Button} from "../../Elements/Button";
+import { Button } from "../../Elements/Button";
 import { useState } from 'react';
 import axios from 'axios';
 
 const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu
 
-export const FeedbackForm = ({setActive}) => {
+export const FeedbackForm = ({ setActive }) => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [text, setText] = useState('')
+    const [checkbox, setCheckbox] = useState(false)
 
     const sendForm = async () => {
-        if (name != '' && email != '' && text != '' && EMAIL_REGEXP.test(email)) {
+        if (name != '' && email != '' && text != '' && EMAIL_REGEXP.test(email) && checkbox) {
 
             let sendData = {
                 name: '',
@@ -25,14 +26,14 @@ export const FeedbackForm = ({setActive}) => {
             sendData.text = await text;
 
             axios.post('http://localhost:7700/form', sendData)
-            .then(function (response) {
-                console.log(response);
-                setActive(false);
-            })
-            .catch(function (error) {
-                console.log(error);
-                setActive(false);
-            });
+                .then(function (response) {
+                    console.log(response);
+                    setActive(false);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    setActive(false);
+                });
         };
     };
 
@@ -40,7 +41,7 @@ export const FeedbackForm = ({setActive}) => {
         <form className={styles.form} onClick={e => e.stopPropagation()} onSubmit={e => e.preventDefault()}>
             <div className={styles.form__close}>
                 <Button type="ghost" onClick={() => setActive(false)}>
-                    <FeatherIcon icon="x"/>
+                    <FeatherIcon icon="x" />
                 </Button>
             </div>
 
@@ -48,12 +49,12 @@ export const FeedbackForm = ({setActive}) => {
 
             <div className={styles.form__row}>
                 <label className={styles.form__text} htmlFor="name">Как вас зовут</label>
-                <input type="text" className={styles.form__input} value={name} onChange={(e) => setName(e.target.value)} required id="name"/>
+                <input type="text" className={styles.form__input} value={name} onChange={(e) => setName(e.target.value)} required id="name" />
             </div>
 
             <div className={styles.form__row}>
                 <label className={styles.form__text} htmlFor="email">Ваш Email</label>
-                <input type="email" className={styles.form__input} value={email} onChange={(e) => setEmail(e.target.value)} required id="email"/>
+                <input type="email" className={styles.form__input} value={email} onChange={(e) => setEmail(e.target.value)} required id="email" />
             </div>
 
             <label className={styles.form__row}>
@@ -63,7 +64,7 @@ export const FeedbackForm = ({setActive}) => {
 
 
             <label className={styles.form__agreement}>
-                <input type="checkbox" className={styles.form__checkbox} required/>
+                <input type="checkbox" onChange={() => setCheckbox(!checkbox)} className={styles.form__checkbox} required />
                 Даю согласие на обработку персональных данных
             </label>
 
