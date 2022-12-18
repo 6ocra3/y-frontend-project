878192eb5c90e080edb6e7c0d26c7600724cba64
JeeEssEm <greendiesel228@gmail.com>
2022-12-18 23:42:48 +0300

    маленький фикс

--- src/static/project/test.js
+++ src/static/project/test.js
@@ -1,12 +1,12 @@
 import projectImg from './../image/image.png';
-import { Makar } from '../bio/makar';
+import { MAKAR } from '../bio/makar';
 import { STACK } from './stack';
 export const TestProject = {
   image: projectImg,
   title: 'Название проекта',
   time: '12:06',
   date: '24.11.2022',
-  author: Makar,
+  author: MAKAR,
   projectType: 'Проект для Академии',
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
