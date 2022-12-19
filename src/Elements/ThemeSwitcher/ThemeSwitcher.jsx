30c8b68725e45309351bb16bdcdb0a0d367e079b
sherstnew <sherstnev.denis.v@gmail.com>
2022-12-19 22:25:38 +0300

    theme fixes + embedded projects list

--- src/Elements/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/Elements/ThemeSwitcher/ThemeSwitcher.jsx
@@ -6,12 +6,24 @@ export const ThemeSwitcher = ({theme, setTheme}) => {
   useEffect(() => {
     let th = localStorage.getItem('th');
     if (th == null) {
-      localStorage.setItem('th', 'dark')
+      if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        localStorage.setItem('th', 'light')
+        document.body.style.backgroundColor = '#ffffff'
+        setTheme('light')
+      } else {
+        localStorage.setItem('th', 'dark')
+        document.body.style.backgroundColor = '#1a1e29'
+        setTheme('dark')
+      }
     } else {
       if (th == 'dark') {
+        document.body.style.backgroundColor = '#1a1e29'
         setTheme('dark')
+        localStorage.setItem('th', 'dark')
       } else if (th == 'light') {
+        document.body.style.backgroundColor = '#ffffff'
         setTheme('light')
+        localStorage.setItem('th', 'light')
       }
     }
   })
@@ -19,9 +31,11 @@ export const ThemeSwitcher = ({theme, setTheme}) => {
   function switchTheme() {
     let th = localStorage.getItem('th');
     if (th == 'dark') {
+      document.body.style.backgroundColor = '#ffffff'
       setTheme('light')
       localStorage.setItem('th', 'light')
     } else if (th == 'light') {
+      document.body.style.backgroundColor = '#1a1e29'
       setTheme('dark')
       localStorage.setItem('th', 'dark')
     }
--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,6 +1,6 @@
 import {ProjectsList} from "../../components/ProjectsList";
 import { ThemeSwitcher } from '../../Elements/ThemeSwitcher/ThemeSwitcher';
-// import styles from "./styles.module.css";
+import styles from "./styles.module.css";
 import {ProjectsListData} from "../../static/project/ProjectsList";
 
 export const ProjectsPage = ({theme, setTheme}) => {
@@ -8,6 +8,7 @@ export const ProjectsPage = ({theme, setTheme}) => {
     return (
         <div>
             <ThemeSwitcher theme={theme} setTheme={setTheme} />
+            <div className={theme == 'light' ? styles.projectCityImage + " " + styles.projectCityImage_light : styles.projectCityImage}></div>
             <ProjectsList theme={theme} projects={ProjectsListData}/>
         </div>
     )
